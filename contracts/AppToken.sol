9f2f4883dfd4ac11e1b70e5036251e6af6fcb46a
jon <jon@younow.com>
2020-11-30 15:59:52 -0500

    after solhint

--- contracts/AppToken.sol
+++ contracts/AppToken.sol
@@ -1,35 +1,41 @@
 pragma solidity ^0.6.8;
 
-import '@openzeppelin/contracts-upgradeable/proxy/Initializable.sol';
-import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 
 contract AppToken is Initializable, OwnableUpgradeable, ERC20Upgradeable {
-    
     using SafeMathUpgradeable for uint256;
-    
+
     uint256 public propsMintPercent = 100000; // denoted as ppm i.e. 10%
+
     /**
-    * @dev App Token's ERC20 Logic contract that proxies point to
-    * @param _name name
-    * @param _owner The address of the thing owner
-    * @param _propsOwner The address of the thing owner
-    * @param _symbol name  
-    * @param _amount Amount of tokens to mint
-    */
-    
-    function initialize(string memory _name, string memory _symbol, uint256 _amount, address _owner, address _propsOwner) public initializer {
+     * @dev App Token's ERC20 Logic contract that proxies point to
+     * @param _name name
+     * @param _owner The address of the thing owner
+     * @param _propsOwner The address of the thing owner
+     * @param _symbol name
+     * @param _amount Amount of tokens to mint
+     */
+
+    function initialize(
+        string memory _name,
+        string memory _symbol,
+        uint256 _amount,
+        address _owner,
+        address _propsOwner
+    ) public initializer {
         OwnableUpgradeable.__Ownable_init();
         ERC20Upgradeable.__ERC20_init(_name, _symbol);
         if (_owner != msg.sender) {
             transferOwnership(_owner);
         }
-        
-        uint256 amountWithDecimals = _amount.mul(10 ** uint256(decimals()));
-        uint256 propsTokens = amountWithDecimals.mul(propsMintPercent).div(1e6);        
-        
+
+        uint256 amountWithDecimals = _amount.mul(10**uint256(decimals()));
+        uint256 propsTokens = amountWithDecimals.mul(propsMintPercent).div(1e6);
+
         _mint(_owner, amountWithDecimals.sub(propsTokens));
-        _mint(_propsOwner, propsTokens);    
+        _mint(_propsOwner, propsTokens);
     }
 }
--- contracts/AppTokenManager.sol
+++ contracts/AppTokenManager.sol
@@ -1,28 +1,41 @@
 pragma solidity ^0.6.8;
 import "./AppToken.sol";
-import '../temp-oz-contracts-for-proxy/upgradeability/ProxyFactory.sol';
-contract AppTokenManager is ProxyFactory {    
+import "../temp-oz-contracts-for-proxy/upgradeability/ProxyFactory.sol";
+
+contract AppTokenManager is ProxyFactory {
     // EVENTS
 
     event AppTokenCreated(address indexed tokenAddress, string name, uint256 amount);
-    
+
     // STORAGE
-    
+
     address public implementationContract;
     address[] public appTokens;
-    
-    
 
-    constructor (address _implementationContract) public {
+    constructor(address _implementationContract) public {
         implementationContract = _implementationContract;
     }
-  
-    function createAppToken(string memory _name, string memory _symbol, uint256 _amount, address _owner, address _propsOwner) public returns (address){
-        bytes memory payload = abi.encodeWithSignature("initialize(string,string,uint256,address,address)", _name, _symbol, _amount, _owner, _propsOwner);
-        address proxy = deployMinimal(implementationContract, payload);        
+
+    function createAppToken(
+        string memory _name,
+        string memory _symbol,
+        uint256 _amount,
+        address _owner,
+        address _propsOwner
+    ) public returns (address) {
+        bytes memory payload =
+            abi.encodeWithSignature(
+                "initialize(string,string,uint256,address,address)",
+                _name,
+                _symbol,
+                _amount,
+                _owner,
+                _propsOwner
+            );
+        address proxy = deployMinimal(implementationContract, payload);
         emit AppTokenCreated(proxy, _name, _amount);
 
         appTokens.push(proxy);
         return proxy;
-    }   
+    }
 }
