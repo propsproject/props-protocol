7b537b386d1b6b8e63e6410b1a644a13b8fc18e1
George Roman <george.roman.99@gmail.com>
2021-03-11 09:47:05 +0200

    Re-organize post-deployment scripts

--- scripts/post-deployment/post-deploy-l1.ts
+++ scripts/post-deployment/post-deploy-l1.ts
@@ -5,6 +5,11 @@ import { ethers, upgrades } from "hardhat";
 
 import type { AppProxyFactoryL1 } from "../../typechain";
 
+// Matic contracts
+const MATIC_MINTABLE_ERC20_PREDICATE = process.env.TESTNET
+  ? "0x37c3bfC05d5ebF9EBb3FF80ce0bd0133Bf221BC8"
+  : "0x9923263fA127b3d1484cFD649df8f1831c2A74e4";
+
 // Accounts
 let deployer: SignerWithAddress;
 let controller: SignerWithAddress;
@@ -28,13 +33,50 @@ async function main() {
     .transferControl(`${process.env.CONTROLLER_MULTISIG_L1}`)
     .then((tx: ContractTransaction) => tx.wait());
 
-  console.log("Transferring `ProxyAdmin` ownership to ControllerMultisigL1");
+  console.log("Transferring protocol `ProxyAdmin` ownership to ControllerMultisigL1");
   await upgrades.admin.transferProxyAdminOwnership(`${process.env.CONTROLLER_MULTISIG_L1}`);
 
-  // To change the admin via an arbitrary address, run the following:
-  // await (await upgrades.admin.getInstance())
-  //   .connect(controller)
-  //   .transferOwnership(`${process.env.CONTROLLER_MULTISIG_L1}`);
+  // We don't have any permissions to change anything on the L1 Props token,
+  // as that resides as a separate deployment and is controlled by accounts
+  // we don't have access to in here. However, what we can do is prepare any
+  // calls that are to be performed by the controlling accounts for transferring
+  // any ownership roles on the L1 Props token to accounts that we control.
+
+  console.log("Permissioning Matic bridge as minter on `PropsTokenL1`");
+  console.log("To be sent by controller to `PropsTokenL1`:");
+  console.log(`addMinter(${MATIC_MINTABLE_ERC20_PREDICATE})`);
+  console.log(
+    new ethers.utils.Interface(["function addMinter(address)"]).encodeFunctionData("addMinter", [
+      MATIC_MINTABLE_ERC20_PREDICATE,
+    ])
+  );
+
+  console.log("Transferring `PropsTokenL1` control to ControllerMultisigL1");
+  console.log("To be sent by controller to `PropsTokenL1`:");
+  console.log(`updateController(${process.env.CONTROLLER_MULTISIG_L1})`);
+  console.log(
+    new ethers.utils.Interface([
+      "function updateController(address)",
+    ]).encodeFunctionData("updateController", [`${process.env.CONTROLLER_MULTISIG_L1}`])
+  );
+
+  console.log("Transferring `PropsTokenL1` proxy administration to protocol `ProxyAdmin`");
+  console.log(
+    "To be sent by `PropsTokenL1`'s `ProxyAdmin` owner to `PropsTokenL1`'s `ProxyAdmin`:"
+  );
+  console.log(
+    `changeProxyAdmin(${l1Addresses["propsToken"]}, ${
+      (await upgrades.admin.getInstance()).address
+    })`
+  );
+  console.log(
+    new ethers.utils.Interface([
+      "function changeProxyAdmin(address, address)",
+    ]).encodeFunctionData("changeProxyAdmin", [
+      l1Addresses["propsToken"],
+      (await upgrades.admin.getInstance()).address,
+    ])
+  );
 }
 
 main()
--- scripts/post-deployment/post-deploy-l2.ts
+++ scripts/post-deployment/post-deploy-l2.ts
@@ -1,9 +1,13 @@
-import { ContractTransaction } from "@ethersproject/contracts";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import * as fs from "fs";
 import { ethers, upgrades } from "hardhat";
 
 import type { AppProxyFactoryL2, PropsProtocol, PropsTokenL2 } from "../../typechain";
+
+// Matic contracts
+const MATIC_CHILD_CHAIN_MANAGER = process.env.TESTNET
+  ? "0xb5505a6d998549090530911180f38aC5130101c6"
+  : "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa";
 
 // Accounts
 let deployer: SignerWithAddress;
@@ -21,14 +25,20 @@ async function main() {
     fs.readFileSync(`deployments/${process.env.L2_NETWORK}.json`).toString()
   ).slice(-1)[0];
 
-  console.log("Transferring `PropsTokenL2` ownership to ControllerMultisigL2");
+  console.log("Permissioning Matic bridge as minter on `PropsTokenL2`");
   propsToken = (await ethers.getContractFactory("PropsTokenL2", deployer)).attach(
     l2Addresses["propsToken"]
   ) as PropsTokenL2;
   await propsToken
     .connect(controller)
+    .addMinter(MATIC_CHILD_CHAIN_MANAGER)
+    .then((tx) => tx.wait());
+
+  console.log("Transferring `PropsTokenL2` ownership to ControllerMultisigL2");
+  await propsToken
+    .connect(controller)
     .transferOwnership(`${process.env.CONTROLLER_MULTISIG_L2}`)
-    .then((tx: ContractTransaction) => tx.wait());
+    .then((tx) => tx.wait());
 
   console.log("Transferring `AppProxyFactoryL2` control to ControllerMultisigL2");
   appProxyFactory = (await ethers.getContractFactory("AppProxyFactoryL2", deployer)).attach(
@@ -37,7 +47,7 @@ async function main() {
   await appProxyFactory
     .connect(controller)
     .transferControl(`${process.env.CONTROLLER_MULTISIG_L2}`)
-    .then((tx: ContractTransaction) => tx.wait());
+    .then((tx) => tx.wait());
 
   console.log("Transferring `PropsProtocol` control to ControllerMultisigL2");
   propsProtocol = (await ethers.getContractFactory("PropsProtocol", deployer)).attach(
@@ -46,15 +56,10 @@ async function main() {
   await propsProtocol
     .connect(controller)
     .transferControl(`${process.env.CONTROLLER_MULTISIG_L2}`)
-    .then((tx: ContractTransaction) => tx.wait());
+    .then((tx) => tx.wait());
 
-  console.log("Transferring `ProxyAdmin` ownership to ControllerMultisigL2");
+  console.log("Transferring protocol `ProxyAdmin` ownership to ControllerMultisigL2");
   await upgrades.admin.transferProxyAdminOwnership(`${process.env.CONTROLLER_MULTISIG_L2}`);
-
-  // To change the admin via an arbitrary address, run the following:
-  // await (await upgrades.admin.getInstance())
-  //   .connect(controller)
-  //   .transferOwnership(`${process.env.CONTROLLER_MULTISIG_L2}`);
 }
 
 main()
