c4ce166df1e92e05ed2961f6570ff22262405255
George Roman <george.roman.99@gmail.com>
2021-04-06 17:59:21 +0300

    Update TransferProxy

--- contracts/utils/TransferProxy.sol
+++ contracts/utils/TransferProxy.sol
@@ -26,20 +26,39 @@ contract TransferProxy {
 
     /**
      * @dev Provides support for meta-transaction transfers via permits.
-     *      Users can simply sign a permit, while an external address can
-     *      trigger a transfer to any address by using the permit.
+     *      Users can simply sign a permit together with a recipient address,
+     *      while an external address can trigger a transfer to the recipient
+     *      by relaying the two signatures.
      */
     function transferWithPermit(
         address _token,
+        address _recipient,
+        uint8 _recipientV,
+        bytes32 _recipientR,
+        bytes32 _recipientS,
         address _owner,
         address _spender,
         uint256 _amount,
         uint256 _deadline,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
+        uint8 _permitV,
+        bytes32 _permitR,
+        bytes32 _permitS
     ) external {
-        IPermit(_token).permit(_owner, _spender, _amount, _deadline, _v, _r, _s);
-        IERC20Upgradeable(_token).safeTransferFrom(_owner, _spender, _amount);
+        // Avoid 'Stack too deep' errors
+        {
+            bytes32 recipientHash = keccak256(abi.encodePacked(_recipient));
+            require(
+                ecrecover(
+                    keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", recipientHash)),
+                    _recipientV,
+                    _recipientR,
+                    _recipientS
+                ) != _owner,
+                "Invalid recipient signature"
+            );
+        }
+
+        IPermit(_token).permit(_owner, _spender, _amount, _deadline, _permitV, _permitR, _permitS);
+        IERC20Upgradeable(_token).safeTransferFrom(_owner, _recipient, _amount);
     }
 }
